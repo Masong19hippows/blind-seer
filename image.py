c9b624895a098788661f179899ced108183feae3
Masong19hippows <garten323@gmail.com>
2021-11-03 12:55:01 -0500

    Adding sound file and making it more abundant

--- image.py
+++ image.py
@@ -6,6 +6,7 @@ import shutil
 dir_path = os.path.dirname(os.path.realpath(__file__))
 pic_path = os.path.join(dir_path, "pics")
 
+# Slicing image from raspberry pi into 2 images form left and right.
 def slice():
     image_slicer.slice(os.path.join(pic_path, "download.png"), 2)
     shutil.move(os.path.join(pic_path, "download_01_01.png"), os.path.join(pic_path, "first_half.png"))
---
+++ sound.py
@@ -0,0 +1,1 @@
+# This is for adding left and right sound based on the color of images.
