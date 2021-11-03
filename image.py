2639d09c0bc0d011f5415e74c510a834d33e134f
Masong19hippows <garten323@gmail.com>
2021-11-03 12:52:07 -0500

    Testing

--- image.py
+++ image.py
@@ -1,1 +1,13 @@
-print("tets")
+import image_slicer
+import os
+import shutil
+
+# Setting Dirsectory for future use
+dir_path = os.path.dirname(os.path.realpath(__file__))
+pic_path = os.path.join(dir_path, "pics")
+
+def slice():
+    image_slicer.slice(os.path.join(pic_path, "download.png"), 2)
+    shutil.move(os.path.join(pic_path, "download_01_01.png"), os.path.join(pic_path, "first_half.png"))
+    shutil.move(os.path.join(pic_path, "download_01_02.png"), os.path.join(pic_path, "second_half.png"))
+slice()
