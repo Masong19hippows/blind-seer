2c41fe6c7b772d66987191c007b8b8a81702763d
Masong19hippows <garten323@gmail.com>
2021-11-29 23:09:04 -0600

    Final

--- image.py
+++ image.py
@@ -2,7 +2,8 @@ import image_slicer
 import os
 import shutil
 import webcolors
-from skimage import io
+import sound
+import time
 from colorthief import ColorThief
 
 # from picamera import PiCamera
@@ -11,17 +12,17 @@ from colorthief import ColorThief
 dir_path = os.path.dirname(os.path.realpath(__file__))
 pic_path = os.path.join(dir_path, "pics")
 
-# def get_image():
-#     camera = PiCamera()
-#     time.sleep(2)
-#     camera.capture(os.path.join(pic_path, "img.jpg"))
+def get_image():
+    camera = PiCamera()
+    time.sleep(2)
+    camera.capture(os.path.join(pic_path, "img.jpg"))
 
 # Slicing image from raspberry pi into 2 images from left to right.
 def slice():
-    # get_image()
-    image_slicer.slice(os.path.join(pic_path, "img.png"), 2)
-    shutil.move(os.path.join(pic_path, "img_01_01.png"), os.path.join(pic_path, "first_half.png"))
-    shutil.move(os.path.join(pic_path, "img_01_02.png"), os.path.join(pic_path, "second_half.png"))
+    get_image()
+    image_slicer.slice(os.path.join(pic_path, "img.jpg"), 2)
+    shutil.move(os.path.join(pic_path, "img_01_01.png"), os.path.join(pic_path, "left_half.png"))
+    shutil.move(os.path.join(pic_path, "img_01_02.png"), os.path.join(pic_path, "right_half.png"))
 
   
 # Getting the most dominant color (using k-means clustering) in the 2 images and outputting it as rgb values for both left and right
@@ -29,27 +30,25 @@ def get_color():
 
     # Slicing the image and setting variables that refrence both the split images
     slice()
-    first_rgb = ColorThief(os.path.join(pic_path, "first_half.png")).get_color(quality=1)
-    second_rgb = ColorThief(os.path.join(pic_path, "second_half.png")).get_color(quality=1)
-    test = []
+    left_rgb = ColorThief(os.path.join(pic_path, "left_half.png")).get_color(quality=1)
+    right_rgb = ColorThief(os.path.join(pic_path, "right_half.png")).get_color(quality=1)
+    colors = []
     min_colours = {}
     for key, name in webcolors.CSS3_HEX_TO_NAMES.items():
         r_c, g_c, b_c = webcolors.hex_to_rgb(key)
-        rd = (r_c - first_rgb[0]) ** 2
-        gd = (g_c - first_rgb[1]) ** 2
-        bd = (b_c - first_rgb[2]) ** 2
+        rd = (r_c - left_rgb[0]) ** 2
+        gd = (g_c - left_rgb[1]) ** 2
+        bd = (b_c - left_rgb[2]) ** 2
         min_colours[(rd + gd + bd)] = name
-    test.append(min_colours[min(min_colours.keys())])
+    colors.append(min_colours[min(min_colours.keys())])
     for key, name in webcolors.CSS3_HEX_TO_NAMES.items():
         r_c, g_c, b_c = webcolors.hex_to_rgb(key)
-        rd = (r_c - second_rgb[0]) ** 2
-        gd = (g_c - second_rgb[1]) ** 2
-        bd = (b_c - second_rgb[2]) ** 2
+        rd = (r_c - right_rgb[0]) ** 2
+        gd = (g_c - right_rgb[1]) ** 2
+        bd = (b_c - right_rgb[2]) ** 2
         min_colours[(rd + gd + bd)] = name
-    test.append(min_colours[min(min_colours.keys())])
+    colors.append(min_colours[min(min_colours.keys())])
 
-    return test
-
-
-
-print(get_color())
+    return colors
+while True:
+    sound.play(get_color())
--- sound.py
+++ sound.py
@@ -1,23 +1,33 @@
 import image
-import pygame
 import os
-import soundfile
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import pygame
+import time
+from pydub import AudioSegment
 from gtts import gTTS
 
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
+sound_path = os.path.join(dir_path, "pics")
 
-def play():
-    pygame.mixer.init() 
-    color = image.get_color()
-    tts = gTTS(text=color[0], lang='en')
-    tts.save("colorleft.wav")
-    tts = gTTS(text=color[1], lang='en')
-    tts.save("colorright.wav")
+def play(colors):
+    tts = gTTS(text=colors[0], lang='en')
+    tts.save(os.path.join(sound_path, "colorleftOG.wav"))
+    tts = gTTS(text=colors[1], lang='en')
+    tts.save(os.path.join(sound_path, "colorrightOG.wav"))
+    os.system(f"ffmpeg -y -i {os.path.join(sound_path, 'colorleftOG.wav')} {os.path.join(sound_path, 'colorleft.wav')}")
+    os.system(f"ffmpeg -y -i {os.path.join(sound_path, 'colorrightOG.wav')} {os.path.join(sound_path, 'colorright.wav')}")
+
     pygame.mixer.init(frequency=44000, size=-16,channels=2, buffer=4096)
-    pygame.mixer.set_num_channels(2)
-    m = pygame.mixer.Sound(os.path.join(dir_path, 'colorleft.wav'))
-    n = pygame.mixer.Sound(os.path.join(dir_path, 'colorright.wav'))
-    pygame.mixer.Channel(1).play(m,-1)
-    pygame.mixer.Channel(2).play(n,-1)
-play()
+
+    sound0 = pygame.mixer.Sound(os.path.join(sound_path, 'colorleft.wav'))
+    sound1 = pygame.mixer.Sound(os.path.join(sound_path, 'colorright.wav'))
+
+    pygame.mixer.Channel(1).set_volume(1.0, 0.0)
+    pygame.mixer.Channel(2).set_volume(0.0, 1.0)
+    pygame.mixer.Channel(1).play(sound0)
+    while pygame.mixer.Channel(1).get_busy():
+        pygame.time.delay(100)
+    pygame.mixer.Channel(2).play(sound1)
+    while pygame.mixer.Channel(2).get_busy():
+        pygame.time.delay(100)
