f8f5f32013dc38b129087072d67d80fa96c8ecda
Masong19hippows <garten323@gmail.com>
2021-11-29 21:43:00 -0600

    Final testing

--- image.py
+++ image.py
@@ -1,49 +1,55 @@
 import image_slicer
 import os
 import shutil
-import cv2
-import time
-import numpy as np
+import webcolors
 from skimage import io
-from picamera import PiCamera
+from colorthief import ColorThief
+
+# from picamera import PiCamera
 
 # Setting directories for future use
 dir_path = os.path.dirname(os.path.realpath(__file__))
 pic_path = os.path.join(dir_path, "pics")
 
-def get_image():
-    camera = PiCamera()
-    time.sleep(2)
-    camera.capture(os.path.join(pic_path, "img.jpg"))
+# def get_image():
+#     camera = PiCamera()
+#     time.sleep(2)
+#     camera.capture(os.path.join(pic_path, "img.jpg"))
 
 # Slicing image from raspberry pi into 2 images from left to right.
 def slice():
-    get_image()
-    image_slicer.slice(os.path.join(pic_path, "img.jpg"), 2)
+    # get_image()
+    image_slicer.slice(os.path.join(pic_path, "img.png"), 2)
     shutil.move(os.path.join(pic_path, "img_01_01.png"), os.path.join(pic_path, "first_half.png"))
     shutil.move(os.path.join(pic_path, "img_01_02.png"), os.path.join(pic_path, "second_half.png"))
 
-
+  
 # Getting the most dominant color (using k-means clustering) in the 2 images and outputting it as rgb values for both left and right
 def get_color():
 
     # Slicing the image and setting variables that refrence both the split images
     slice()
-    img1 = io.imread(os.path.join(pic_path, "first_half.png"))[:, :, :-1]
-    img2 = io.imread(os.path.join(pic_path, "second_half.png"))[:, :, :-1]
+    first_rgb = ColorThief(os.path.join(pic_path, "first_half.png")).get_color(quality=1)
+    second_rgb = ColorThief(os.path.join(pic_path, "second_half.png")).get_color(quality=1)
+    test = []
+    min_colours = {}
+    for key, name in webcolors.CSS3_HEX_TO_NAMES.items():
+        r_c, g_c, b_c = webcolors.hex_to_rgb(key)
+        rd = (r_c - first_rgb[0]) ** 2
+        gd = (g_c - first_rgb[1]) ** 2
+        bd = (b_c - first_rgb[2]) ** 2
+        min_colours[(rd + gd + bd)] = name
+    test.append(min_colours[min(min_colours.keys())])
+    for key, name in webcolors.CSS3_HEX_TO_NAMES.items():
+        r_c, g_c, b_c = webcolors.hex_to_rgb(key)
+        rd = (r_c - second_rgb[0]) ** 2
+        gd = (g_c - second_rgb[1]) ** 2
+        bd = (b_c - second_rgb[2]) ** 2
+        min_colours[(rd + gd + bd)] = name
+    test.append(min_colours[min(min_colours.keys())])
 
-    # Getting the dominant color of both images
-    pixels1 = np.float32(img1.reshape(-1, 3))
-    pixels2 = np.float32(img2.reshape(-1, 3))
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 200, .1)
-    flags = cv2.KMEANS_RANDOM_CENTERS
-    _, labels1, palette1 = cv2.kmeans(pixels1, 5, None, criteria, 10, flags)
-    _, labels2, palette2 = cv2.kmeans(pixels2, 5, None, criteria, 10, flags)
-    _, counts1 = np.unique(labels1, return_counts=True)
-    _, counts2 = np.unique(labels2, return_counts=True)
+    return test
 
-    # Returning the domanint color in both images in a RGB value list
-    list = np.append(palette1[np.argmax(counts1)], palette2[np.argmax(counts2)])
-    return list
-    
+
+
 print(get_color())
