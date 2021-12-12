7609930c544928ac27b441cc3726647682421190
masong19hippows <garten323@gmail.com>
2021-12-11 19:06:48 -0600

    Making faster and removing nnetwork requirement

--- image.py
+++ image.py
@@ -4,54 +4,53 @@ import time
 import sound
 import threading
 import shutil
-import webcolors
-from colorthief import ColorThief
-from picamera import PiCamera
+from PIL import Image
 from PIL import UnidentifiedImageError
 from math import sqrt
+# from picamera import PiCamera
 
 # Setting directories for future use
 dir_path = os.path.dirname(os.path.realpath(__file__))
 pic_path = os.path.join(dir_path, "pics")
 
-def get_image():
-    camera = PiCamera()
-    camera.resolution = (320, 240)
-    for i in enumerate(camera.capture_continuous(os.path.join(pic_path, "img.jpg"), use_video_port=False)):
-            time.sleep(4)
+# def get_image():
+    # camera = PiCamera()
+    # camera.resolution = (320, 240)
+    # for i in enumerate(camera.capture_continuous(os.path.join(pic_path, "img.jpg"), use_video_port=False)):
+    #         time.sleep(4)
 
 # Slicing image from raspberry pi into 2 images from left to right.
 def slice():
     try:
-        image_slicer.slice(os.path.join(pic_path, "img.jpg"), 2)
+        image_slicer.slice(os.path.join(pic_path, "img.png"), 2)
     except UnidentifiedImageError:
-        time.sleep(.5)
-        image_slicer.slice(os.path.join(pic_path, "img.jpg"), 2)
+        time.sleep(.25)
+        image_slicer.slice(os.path.join(pic_path, "img.png"), 2)
         
     shutil.move(os.path.join(pic_path, "img_01_01.png"), os.path.join(pic_path, "left_half.png"))
     shutil.move(os.path.join(pic_path, "img_01_02.png"), os.path.join(pic_path, "right_half.png"))
 
 Colors = {
-    'Black': (0, 0, 0),
-    'Gray': (127, 127, 127),
-    'Bordeaux': (136, 0, 21),
+    'black': (0, 0, 0),
+    'gray': (127, 127, 127),
+    'bordeaux': (136, 0, 21),
     'red': (237, 28, 36), 
     'orange': (255, 127, 39),
     'yellow': (255, 242, 0),
     'green': (34, 177, 76),
     'blue': (203, 228, 253),
-    'dark blue': (0, 162, 232),
+    'dark_blue': (0, 162, 232),
     'purple': (63, 72, 204),
     'white': (255, 255, 255),
-    'light gray': (195, 195, 195),
-    'light brown': (185, 122, 87),
-    'light pink': (255, 174, 201),
-    'dark yellow': (255, 201, 14),
-    'light yellow': (239, 228, 176),
-    'light green': (181, 230, 29),
-    'light blue': (153, 217, 234),
-    'dark blue': (112, 146, 190),
-    'light purple': (200, 191, 231)
+    'light_gray': (195, 195, 195),
+    'light_brown': (185, 122, 87),
+    'light_pink': (255, 174, 201),
+    'dark_yellow': (255, 201, 14),
+    'light_yellow': (239, 228, 176),
+    'light_green': (181, 230, 29),
+    'light_blue': (153, 217, 234),
+    'dark_blue': (112, 146, 190),
+    'light_purple': (200, 191, 231)
 }
 
 def closest_color(rgb):
@@ -67,15 +66,33 @@ def closest_color(rgb):
             return key
     return None
 
-# Getting the most dominant color (using k-means clustering) in the 2 images and outputting it as rgb values for both left and right
+# Getting the most dominant color in the 2 images and outputting it as rgb values for both left and right
 def get_colors():
     # Slicing the image and setting variables that refrence both the split images
     slice()
-    left_rgb = ColorThief(os.path.join(pic_path, "left_half.png")).get_color(quality=1)
-    right_rgb = ColorThief(os.path.join(pic_path, "right_half.png")).get_color(quality=1)
+
     colors = []
-    colors.append(closest_color(left_rgb))
-    colors.append(closest_color(right_rgb))
+    with Image.open(os.path.join(pic_path, "left_half.png")) as img:
+        img.thumbnail((100, 100))
+        # Reduce colors (uses k-means internally)
+        paletted = img.convert('P', palette=Image.ADAPTIVE, colors=16)
+        # Find the color that occurs most often
+        palette = paletted.getpalette()
+        color_counts = sorted(paletted.getcolors(), reverse=True)
+        palette_index = color_counts[0][1]
+        dominant_color = palette[palette_index * 3:palette_index*3 + 3]
+        colors.append(closest_color(dominant_color))
+
+    with Image.open(os.path.join(pic_path, "right_half.png")) as img:
+        img.thumbnail((100, 100))
+        # Reduce colors (uses k-means internally)
+        paletted = img.convert('P', palette=Image.ADAPTIVE, colors=16)
+        # Find the color that occurs most often
+        palette = paletted.getpalette()
+        color_counts = sorted(paletted.getcolors(), reverse=True)
+        palette_index = color_counts[0][1]
+        dominant_color = palette[palette_index* 3:palette_index*3 + 3]
+        colors.append(closest_color(dominant_color))
 
     return colors
 
@@ -84,9 +101,9 @@ def loop():
     while True:
         sound.play(get_colors())
 
-t1 = threading.Thread(target=get_image, name='t1')
+# t1 = threading.Thread(target=get_image, name='t1')
 t2 = threading.Thread(target=loop, name='t2')
-t1.start()
+# t1.start()
 t2.start()
 
 
--- sound.py
+++ sound.py
@@ -1,30 +1,30 @@
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
-from gtts import gTTS
 
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-sound_path = os.path.join(dir_path, "pics")
+sound_path = os.path.join(dir_path, "sounds")
 
 def play(colors):
-    tts = gTTS(text=colors[0], lang='en',  slow=False)
-    tts.save(os.path.join(sound_path, "colorleftOG.wav"))
-    tts = gTTS(text=colors[1], lang='en',  slow=False)
-    tts.save(os.path.join(sound_path, "colorrightOG.wav"))
-    os.system(f"ffmpeg -y -i {os.path.join(sound_path, 'colorleftOG.wav')} {os.path.join(sound_path, 'colorleft.wav')}  > /dev/null 2>&1")
-    os.system(f"ffmpeg -y -i {os.path.join(sound_path, 'colorrightOG.wav')} {os.path.join(sound_path, 'colorright.wav')}  > /dev/null 2>&1")
+
+    for file in os.listdir(sound_path):
+        file_name = os.path.splitext(file)[0]
+        if file_name == colors[0]:
+            left = os.path.join(sound_path, file)
+        if file_name == colors[1]:
+            right = os.path.join(sound_path, file)
 
     pygame.mixer.init(frequency=44000, size=-16,channels=2, buffer=4096)
 
-    sound0 = pygame.mixer.Sound(os.path.join(sound_path, 'colorleft.wav'))
-    sound1 = pygame.mixer.Sound(os.path.join(sound_path, 'colorright.wav'))
+    sound0 = pygame.mixer.Sound(left)
+    sound1 = pygame.mixer.Sound(right)
 
     pygame.mixer.Channel(1).set_volume(1.0, 0.0)
     pygame.mixer.Channel(2).set_volume(0.0, 1.0)
     pygame.mixer.Channel(1).play(sound0)
     while pygame.mixer.Channel(1).get_busy():
-        pygame.time.delay(10)
+        pygame.time.delay(5)
     pygame.mixer.Channel(2).play(sound1)
     while pygame.mixer.Channel(2).get_busy():
-        pygame.time.delay(10)
+        pygame.time.delay(5)
