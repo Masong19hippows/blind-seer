c9adf65f4f04e0961e2a2ecfd8d4f54a3df8e03f
masong19hippows <garten323@gmail.com>
2022-04-22 00:17:50 -0500

    Adding button support

--- image.py
+++ image.py
@@ -56,7 +56,7 @@ def loop():
                 time.sleep(3)
                 break
   
-            sound.play(colors.get_colors(), True)
+            sound.play(colors.get_colors())
             time.sleep(.2)
         while True:
 
