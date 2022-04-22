0f5cedf8162d5dd15bc03bd1509b9807058298b6
masong19hippows <garten323@gmail.com>
2022-04-22 00:44:35 -0500

    Adding button support

--- detect.py
+++ detect.py
@@ -8,59 +8,57 @@ pic_path = os.path.join(dir_path, "pics")
 img_path = os.path.join(dir_path, "detect_cfg")
 
 def detect():
-    # def get_output_layers(net):
+    def get_output_layers(net):
         
-    #     layer_names = net.getLayerNames()
+        layer_names = net.getLayerNames()
         
-    #     output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+        output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
 
-    #     return output_layers
+        return output_layers
 
 
 
 
         
-    # image = cv2.imread(os.path.join(pic_path, "img.jpg"))
+    image = cv2.imread(os.path.join(pic_path, "img.jpg"))
 
-    # Width = image.shape[1]
-    # Height = image.shape[0]
-    # scale = 0.00392
+    Width = image.shape[1]
+    Height = image.shape[0]
+    scale = 0.00392
 
-    # classes = None
+    classes = None
 
-    # with open(os.path.join(img_path, "yolov3.txt"), 'r') as f:
-    #     classes = [line.strip() for line in f.readlines()]
+    with open(os.path.join(img_path, "yolov3.txt"), 'r') as f:
+        classes = [line.strip() for line in f.readlines()]
 
-    # net = cv2.dnn.readNet(os.path.join(img_path, "yolov3.weights"), os.path.join(img_path, "yolov3.cfg"))
+    net = cv2.dnn.readNet(os.path.join(img_path, "yolov3.weights"), os.path.join(img_path, "yolov3.cfg"))
 
-    # blob = cv2.dnn.blobFromImage(image, scale, (416,416), (0,0,0), True, crop=False)
+    blob = cv2.dnn.blobFromImage(image, scale, (416,416), (0,0,0), True, crop=False)
 
-    # net.setInput(blob)
+    net.setInput(blob)
 
-    # outs = net.forward(get_output_layers(net))
+    outs = net.forward(get_output_layers(net))
 
-    # class_ids = []
-    # confidences = []
+    class_ids = []
+    confidences = []
 
 
-    # for out in outs:
-    #     for detection in out:
-    #         scores = detection[5:]
-    #         class_id = np.argmax(scores)
-    #         confidence = scores[class_id]
-    #         if confidence > 0.5:
-    #             class_ids.append(class_id)
-    #             confidences.append(float(confidence))
-    # # last = -1
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                class_ids.append(class_id)
+                confidences.append(float(confidence))
+    last = -1
     name = None
-    # for i in range(len(class_ids)):
-    #     if confidences[i] > last:
-    #         last = confidences[i]
-    #         name = i
+    for i in range(len(class_ids)):
+        if confidences[i] > last:
+            last = confidences[i]
+            name = i
 
-    if name == None:
-        return ["test", "test"]
-
-        # return [classes[class_ids[name]], classes[class_ids[name]]]
+    if name != None:
+        return [classes[class_ids[name]], classes[class_ids[name]]]
     else:
         return ["None", "None"]
